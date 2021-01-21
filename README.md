7a709ed22512bedf585026ed85e7f7c0b1a553d7
Michael-Yongshi <4registration@outlook.com>
2021-01-21 13:27:12 +0100

    first draft, dont have gpio hardware connected yet

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # PyDoorbell
+
+## install a raspbian pi
+install pip
+```
+sudo apt install python3-pip
+```
+
+## dependencies
+install requirements
+```
+pip3 install playsound && pip3 install gpiozero && pip3 install rpi.gpio
+```
+
+or run the requirements file
+```
+pip3 install -r requirements.txt
+```
+
+## copy repo
+copy files from other pc (or install git and clone the repo)
+```
+scp pc:~/Doorbell/* ~/
+```
+
+## systemd script
+on the pi, mv the service to systemd
+```
+sudo mv ~/doorbell.service /etc/systemd/system/
+```
+
+enable the script
+```
+sudo systemctl enable doorbell.service
+sudo systemctl start doorbell.service
+```
---
+++ doorbell.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# Input-output and time function for delay, audio library and system commands
+from time import sleep
+from os import system
+import logging
+
+# for handling the GPIO connections
+from gpiozero import LED, Button
+from signal import pause
+
+# for sounds
+from playsound import playsound
+
+# set GPIO connection used for the button (with hold time) and led
+button = Button(18, hold_time=2.5)
+led = LED(4)
+
+def doorbell():
+
+    # set the functions to be run by the button
+    button.when_pressed = button_pressed
+    button.when_held = button_held
+    button.when_released = button_released
+    
+    # Listen for events of the button
+    print(f"listening for visitors")
+    pause()
+
+def button_pressed():
+    print("Button was pressed")
+
+    # turn on led to signal the visitor
+    led.on()
+
+    # play music
+    playsound('myfile.wav')
+
+    # a break to prevent impatient visitors pressing to quickly
+    # (playsound blocks the thread, so if a long sound / song adjust the break time accordingly!)
+    sleep(10.0)
+
+    # turn off led to signal the visitor ringing has ended
+    led.off()
+
+def button_held():
+    print("Button was held")
+
+def button_released():
+    print("Button was released")
+
+if __name__ == "__main__":
+    
+    try:
+        doorbell()
+    finally:
+        print(f"doorbell error!")
